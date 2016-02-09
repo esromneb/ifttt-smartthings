dc2157ce27ae33d77f2497552f03d51902c95557
Kraken <bjmorse@gmail.com>
2016-02-09 00:31:12 -0800

    works with no loop

--- scrape.py
+++ scrape.py
@@ -11,89 +11,89 @@ def curl_unlock():
     key = os.environ['IFFFF']
     os.system('curl -X POST https://maker.ifttt.com/trigger/fart/with/key/'+key)
 
-path = ['issues', 'issues.1', 'issues.2', 'issues.3']
-for delme in path:
+def delete_files():
+    path = ['issues', 'issues.1', 'issues.2', 'issues.3']
+    for delme in path:
+        try:
+            os.remove(delme)
+        except OSError:
+            pass
+delete_files()
+
+def download():
+    # blocking
+    os.system('wget https://github.com/esromneb/legendary-carnival/issues')
+
+    # os.system('cat issues | grep "ba"')
+
     try:
-        os.remove(delme)
-    except OSError:
-        pass
+        f = open('issues', 'r')
+    except IOError:
+        print "OH SHIT NO DOWNload"
+        exit(0)
+        #return
+
+    nextline = False
+
+    saveline = []
 
 
-# blocking
-os.system('wget https://github.com/esromneb/legendary-carnival/issues')
+    for x in f:
+        if nextline:
+            saveline.append(x)
+            nextline = False
 
-# os.system('cat issues | grep "ba"')
+        # print x,
 
-try:
-    f = open('issues', 'r')
-except IOError:
-    print "OH SHIT NO DOWNload"
-    exit(0)
-    #return
-
-nextline = False
-
-saveline = []
+        if 'issue-title-link' in x:
+            nextline = True
 
 
-for x in f:
-    if nextline:
-        saveline.append(x)
-        nextline = False
-
-    # print x,
-
-    if 'issue-title-link' in x:
-        nextline = True
-
-# print "saved this:"
-# print saveline
-
-print "found", len(saveline), "events"
-
-# saveline.reverse()
-
-# print saveline
-
-nickhome = None
-benhome = None
+    print "found", len(saveline), "events"
+    return saveline
 
 
 
-# loop Ben
-for x in saveline:
-    if "aa" in x:
-        benhome = True
-        break
-    if "ab" in x:
-        benhome = False
-        break
-
-# loop
-for x in saveline:
-    # print x
-    if "bb" in x:
-        nickhome = True
-        break
-    if "ba" in x:
-        nickhome = False
+def makeDecision(saveline):
+    nickhome = None
+    benhome = None
 
 
-if benhome is None or nickhome is None:
-    print "PANICK, something went wrong"
+    # loop Ben
+    for x in saveline:
+        if "aa" in x:
+            benhome = True
+            break
+        if "ab" in x:
+            benhome = False
+            break
 
-print "Ben is home", benhome
-print "Nick is home", nickhome
-
-if( benhome or nickhome ):
-    print "LOCKING DOOR"
-    curl_unlock()
-else:
-    print "UNLOCKING DOOR"
-    curl_lock()
-
+    # loop
+    for x in saveline:
+        # print x
+        if "bb" in x:
+            nickhome = True
+            break
+        if "ba" in x:
+            nickhome = False
 
 
+    if benhome is None or nickhome is None:
+        print "PANICK, something went wrong"
+
+    print "Ben is home", benhome
+    print "Nick is home", nickhome
+
+    if( benhome or nickhome ):
+        print "LOCKING DOOR"
+        curl_unlock()
+    else:
+        print "UNLOCKING DOOR"
+        curl_lock()
 
 
+
+print "Start"
+saveoutside = download()
+makeDecision(saveoutside)
 print "done"
