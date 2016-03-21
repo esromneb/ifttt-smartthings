9750010cbdd3ccd762e3d56ef354bed1f4246ff2
pi <bjmorse@gmail.com>
2016-03-21 18:11:42 +1400

    changes

--- listen2.py
+++ listen2.py
@@ -1,6 +1,19 @@
 from scapy.all import *
 import requests
 
+
+
+
+def curl_dash1(dryrun=False):
+    if dryrun:
+        return
+    try:
+        key = os.environ['IFFFF']
+    except:
+        key = ""
+    os.system('curl -X POST https://maker.ifttt.com/trigger/dash1/with/key/'+key)
+
+
 def toggle_st():
     url = 'YOUR_API_ENDPOINT'
     headers = {"Authorization": "Bearer YOUR_API_TOKEN"}
@@ -13,9 +26,9 @@ def arp_display(pkt):
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
       if pkt[ARP].hwsrc == '74:c2:46:cd:56:19': #'00:0c:43:00:5a:ca': # Mac & Cheese
         print "Toggle the light"
-        toggle_st()
+        curl_dash1()
       else:
         print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
-print sniff(prn=arp_display, filter="arp", store=0, count=10)
+print sniff(prn=arp_display, filter="arp", store=0)
 
